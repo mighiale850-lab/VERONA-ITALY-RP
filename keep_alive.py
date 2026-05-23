3bba03630be754ab36677713d3ac574083e7aa4e
mighiale850-lab <mighiale850@gmail.com>
2026-05-23 11:12:14 +0200

    Aggiungi keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "VERONA ITA RP Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
